bc96e6c8f81bcaddebdb322b5254fac70b2565e2
David van Mourik <dcwvanmourik@gmail.com>
2023-02-01 11:54:56 +0100

    Add main method

---
+++ cinema-sofa/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace cinema_sofa
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var movie = new Movie("Hello");
+
+            var screening = new MovieScreening(movie, DateTime.Now, 2.00);
+
+            var ticket = new MovieTicket(screening, true, 2, 1, true);
+
+            var order = new Order(1, true);
+
+            order.AddSeatReservation(ticket);
+
+            order.Export(TicketExportFormat.JSON);
+        }
+    }
+}
+
