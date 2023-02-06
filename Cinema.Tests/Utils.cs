b076ef7f7f1269d9974124b5b31167963e0c48d8
David van Mourik <dcwvanmourik@gmail.com>
2023-02-06 22:12:08 +0100

    Add order tests

---
+++ Cinema.Tests/Utils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Cinema.Tests
+{
+    internal static class Utils
+    {
+
+        internal static int CalculateOffset(DayOfWeek current, DayOfWeek desired)
+        {
+            // f( c, d ) = [7 - (c - d)] mod 7
+            // f( c, d ) = [7 - c + d] mod 7
+            // c is current day of week and 0 <= c < 7
+            // d is desired day of the week and 0 <= d < 7
+            int c = (int)current;
+            int d = (int)desired;
+            int offset = (7 - c + d) % 7;
+            return offset == 0 ? 7 : offset;
+        }
+    }
+}
+
